2765dcd13246ba3d07f5662adfecc4ee84d2ea71
Abhi Sinha <sinha45@purdue.edu>
2019-09-27 21:47:31 -0400

    player statistics

--- src/main/java/game/fb/simulation/models/RunningbackStats.java
+++ src/main/java/game/fb/simulation/models/RunningbackStats.java
@@ -1,4 +1,21 @@
 package main.java.game.fb.simulation.models;
 
-public class RunningbackStats {
+/**
+ * Runningback statistics for a player.
+ * Statistic "number" corresponds to the following:
+ *  0. Agility
+ *  1. Strength
+ *  2. Speed
+ *  3. Vision
+ *
+ * @author abhi
+ */
+public class RunningbackStats implements PositionStats {
+
+    private Position pos = Position.RUNNINGBACK;
+
+    @Override
+    public Position getPosition() {
+        return pos;
+    }
 }
